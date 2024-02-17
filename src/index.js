dfd6da34c37649ebb6f5f2f038163fbce551d4b5
quocvu003 <nguyenquocvu10a2@gmail.com>
2024-02-17 10:58:54 +0700

    rm css

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const express = require('express')
+const { engine } = require('express-handlebars')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+app.use(express.static('./src/public/'))
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine(
+  'hbs',
+  engine({
+    extname: 'hbs',
+  }),
+)
+app.set('view engine', 'hbs')
+app.set('views', './src/resources/views')
+
+// Router
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
